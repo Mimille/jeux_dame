a2c2a27b11de5a5ac0c1b0522a2236366aa29f76
Unknown <edej@orange.fr>
2017-03-29 09:57:35 +0200

    Oeufs verts et roses

--- js/jquery.js
+++ js/jquery.js
@@ -19,7 +19,7 @@ function creationDamier() {
                 if(i == 0 || i == 1 || i == 2 || i == 3){
                     html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "><img class=vert id=vert" + (i + j) + " src='images/vert.png' alt='image'></td>";
                 }else if(i == 6 || i == 7 || i == 8 || i == 9){
-                    html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "><img id=rose" + (i + j) + " src='images/rose.png' alt='image'></td>";
+                    html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "><img class=rose id=rose" + (i + j) + " src='images/rose.png' alt='image'></td>";
                 }else{
                     html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "></td>";
                 }
@@ -32,13 +32,20 @@ function creationDamier() {
     return html;
 }
 
-//survol de la souris : background red
 function survolSouris(){
     $(".vert").on("mouseover", function () {
        $(this).css("background", "red");
     });
 
     $(".vert").on("mouseout", function () {
+        $(this).css("background", "");
+    });
+
+    $(".rose").on("mouseover", function () {
+        $(this).css("background", "red");
+    });
+
+    $(".rose").on("mouseout", function () {
         $(this).css("background", "");
     });
 }
