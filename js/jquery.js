7c6cbf48c24e1110d4527ef0040992e91c45c692
Unknown <edej@orange.fr>
2017-03-29 09:21:30 +0200

    Refonte de la grille

---
+++ js/jquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $("body").append(creationDamier());
+});
+
+function creationDamier() {
+    var html = "<table>";
+
+    for (var i = 0; i < 10 ; i++){
+        html += "<tr>";
+
+        for(var j = 0; j < 10 ; j++){
+            var casePlusJ = j+1;
+            var casePlusI = i+1;
+
+            if ((j % 2) == (i % 2)){
+                html += "<td id='cell-c" + casePlusJ + "-l" + casePlusI + "'></td>";
+            }else{
+                if(i == 0 || i == 1 || i == 2 || i == 3){
+                    html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "><img id=vert" + (i + j) + " src='images/vert.png' alt='image'></td>";
+                }else if(i == 6 || i == 7 || i == 8 || i == 9){
+                    html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "><img id=rose" + (i + j) + " src='images/rose.png' alt='image'></td>";
+                }else{
+                    html += "<td class='caseNoire' id=cell-c" + casePlusJ + "-l" + casePlusI + "></td>";
+                }
+            }
+        }
+    }
+
+    html +="</table>";
+
+    return html;
+}
